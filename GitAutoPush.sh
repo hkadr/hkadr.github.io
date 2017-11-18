ce2544fd9c67e4499176b2b57b2ea56e2331fe25
hkadr <13760220177@163.com>
2017-11-18 23:33:16 +0800

    auto push

--- GitAutoPush.sh
+++ GitAutoPush.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 ###########################
-echo `date '+%Y-%m-%d %H:%M:%S'`
-`echo Start. Push Code...`
-`cd /cygdrive/d/Code/opensrc/hkadr/hkadr.github.io`
-`git init`
-`git pull`
-`git add --all`
-`git commit -m "auto push"`
-`git push`
+echo `date '+%Y-%m-%d %H:%M:%S'` &&
+`echo Start. Push Code...` &&
+`cd /cygdrive/d/Code/opensrc/hkadr/hkadr.github.io` &&
+`git pull` &&
+`git add --all` &&
+`git commit -m "auto push"` &&
+`git push` &&
 `echo Complete. Relaunching...`
