4aaa8adae5cb5e09833d83ebc2ed34f4a336fae9
hkadr <13760220177@163.com>
2017-11-18 23:19:16 +0800

    made changes

---
+++ GitAutoPush.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+###########################
+#cd /cygdrive/d/Code/opensrc/hkadr/hkadr.github.io
+# switch to branch you want to use
+#git checkout master
+# add all added/modified files
+`git add --all`
+# commit changes
+git commit -am "made changes"
+# push to git remote repository
+git push
+###########################
+echo 
